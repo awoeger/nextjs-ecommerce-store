47c5ac8147279340e7a23bafd77d6f3880ddbd29
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-28 18:57:43 +0200

    Add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
+import { css, Global } from '@emotion/react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::before *::after {
+            box-sizing: border-box;
+          }
+
+          body {
+            margin: 0;
+            padding: 0;
+            font-size: 16px;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+          }
+        `}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
