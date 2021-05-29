f68778bc0ae68494eac8a46b93a8b2ac05a54989
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-29 11:47:09 +0200

    Add content for about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
+import { css, Global } from '@emotion/react';
 import Head from 'next/head';
+import Image from 'next/image';
 import Layout from '../components/Layout';
+
+const mainContainerAbout = css`
+  display: flex;
+  align-items: center;
+`;
 
 export default function about() {
   return (
@@ -9,6 +16,27 @@ export default function about() {
       </Head>
       <div>
         <h2>About</h2>
+        <p>
+          Quidditch was founded 2005 in the USA and has grown into a popular
+          sport around the world. The International Quidditch Association (IQA)
+          counts more than 40 national governing bodies with hundreds of teams
+          and more than thousands active players worldwide. In Austria the sport
+          is organised by the national governing body Quidditch Austria (QAT).
+        </p>
+        <p>
+          The Vienna Vanguards Quidditch Club was founded 2014, which makes it
+          the oldest and most successful club in the country. The regular
+          trainings take place twice a week outdoors and once a week indoors. We
+          train throughout the entire year.
+        </p>
+      </div>
+      <div>
+        <Image
+          src="/aboutGroupPictureDanubeCup.JPG"
+          alt="Vienna Vanguards Team 2019"
+          width="3823"
+          height="2549"
+        />
       </div>
     </Layout>
   );
