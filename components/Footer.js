964fa50d91ef590341f2c888a85ff3d3e40df010
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-29 09:50:57 +0200

    Add styling footer and header

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,51 @@
+import { css } from '@emotion/react';
 import Link from 'next/link';
+
+const footerContainer = css`
+  background: #182b4f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+`;
+
+const footerLeftContainer = css`
+  margin-left: 20px;
+
+  p {
+    color: white;
+    font-size: 1.1em;
+  }
+`;
+
+const footerRightContainer = css`
+  margin-right: 20px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    margin-left: 30px;
+    font-size: 1.4em;
+  }
+`;
 
 export default function Footer() {
   return (
-    <>
-      <p>Copyright Vienna Vanguards</p>
-      <Link href="https://www.facebook.com/ViennaVanguards">
-        <a>Instagram</a>
-      </Link>
-      <Link href="https://www.instagram.com/viennavanguards/?hl=de">
-        <a>Facebook</a>
-      </Link>
-      <Link href="https://www.tiktok.com/@viennavanguards?lang=de-DE">
-        <a>Tiktok</a>
-      </Link>
-    </>
+    <div css={footerContainer}>
+      <div css={footerLeftContainer}>
+        <p> © Vienna Vanguards Quidditch Club</p>
+      </div>
+      <div css={footerRightContainer}>
+        <Link href="https://www.facebook.com/ViennaVanguards">
+          <a>Instagram</a>
+        </Link>
+        <Link href="https://www.instagram.com/viennavanguards/?hl=de">
+          <a>Facebook</a>
+        </Link>
+        <Link href="https://www.tiktok.com/@viennavanguards?lang=de-DE">
+          <a>Tiktok</a>
+        </Link>
+      </div>
+    </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,68 @@
 import { css } from '@emotion/react';
+import Image from 'next/image';
 import Link from 'next/link';
+
+// import vanguardsBlue from '../pages/_app.js';
+// import vanguardsOrange from '../pages/_app.js';
+
+const headerContainer = css`
+  background: #182b4f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+`;
+
+const headerLeftContainer = css`
+  margin-left: 20px;
+`;
+const headerRightContainer = css`
+  margin-right: 20px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    margin-left: 30px;
+    font-size: 1.4em;
+  }
+
+  a:hover {
+    color: #f39200;
+    font-weight: bold;
+  }
+`;
+
+const logo = css`
+  max-width: 200px;
+`;
 
 export default function Header() {
   return (
-    <div>
-      <p>Logo</p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/products">
-        <a>Shop</a>
-      </Link>
-      <Link href="/about">
-        <a>Cart Icon</a>
-      </Link>
+    <div css={headerContainer}>
+      <div css={headerLeftContainer}>
+        <Link href="/">
+          <a>
+            <Image
+              css={logo}
+              src="/logo.png"
+              alt="logo"
+              width="70"
+              height="70"
+            />
+          </a>
+        </Link>
+      </div>
+      <div css={headerRightContainer}>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/products">
+          <a>Shop</a>
+        </Link>
+        <Link href="/about">
+          <a>Cart Icon</a>
+        </Link>
+      </div>
     </div>
   );
 }
