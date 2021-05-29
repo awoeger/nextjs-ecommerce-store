37f24f7e3997ec0885331a607e9eaa5727e7d74e
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-29 12:31:08 +0200

    Add database infos

---
+++ util/database.js
@@ -0,0 +1,100 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Jersey blue - short sleeves',
+    price: '20',
+    currency: '€',
+    description:
+      'breathable fabric, soft-touch, moisture resistance, quick dry, 100% polyester, unisex cut only',
+    // img source
+  },
+  {
+    id: 2,
+    name: 'Jersey blue - long sleeves',
+    price: '22',
+    currency: '€',
+    description:
+      'breathable fabric, soft-touch, moisture resistance, quick dry, 100% polyester, unisex cut only',
+    // img source
+  },
+  {
+    id: 3,
+    name: 'Hoodie blue',
+    price: '25',
+    currency: '€',
+    description:
+      'Heavyweight three fly fleece cotton rich fabric, soften handle, double layer hood, unisex cut only',
+    // img source
+  },
+  {
+    id: 4,
+    name: 'Hoodie grey',
+    price: '25',
+    currency: '€',
+    description:
+      'Heavyweight three fly fleece cotton rich fabric, soften handle, double layer hood, unisex cut only',
+    // img source
+  },
+  {
+    id: 5,
+    name: 'Softshell Jacket blue',
+    price: '30',
+    currency: '€',
+    description:
+      '100% polyester bonded fabric, water repellent, wind proof, thermal fabric, resistant to mechanical damage, durable, unisex cut only',
+    // img source
+  },
+  {
+    id: 6,
+    name: 'Pride Jersey - short sleeves',
+    price: '20',
+    currency: '€',
+    description:
+      'breathable fabric, soft-touch, moisture resistance, quick dry, 100% polyester, unisex cut only',
+    // img source
+  },
+  {
+    id: 7,
+    name: 'Zipper Hoodie grey',
+    price: '25',
+    currency: '€',
+    description:
+      'Heavyweight three fly fleece cotton rich fabric, soften handle, double layer hood, unisex cut only',
+    // img source
+  },
+  {
+    id: 8,
+    name: 'T-shirt blue',
+    price: '20',
+    currency: '€',
+    description:
+      '100% cotton fabric, durable, lightweight, soft-touch, unisex cut only',
+    // img source
+  },
+  {
+    id: 9,
+    name: 'Gym bag',
+    price: '15',
+    currency: '€',
+    description: '100% cotton fabric, durable',
+    // img source
+  },
+  {
+    id: 11,
+    name: 'Cap',
+    price: '10',
+    currency: '€',
+    description:
+      '100% chino cotton twill, single size, adjustable strap with hook and pile',
+    // img source
+  },
+  {
+    id: 12,
+    name: 'Headband',
+    price: '6',
+    currency: '€',
+    description:
+      '100% polyester, double-sided, breathable fabric, quick dry, single size',
+    // img source
+  },
+];
