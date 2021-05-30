18c8439a57cb0f5c2cd0afe301c32553c299c20a
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-30 09:38:46 +0200

    Add styling to Homepage

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,57 @@ import { css, Global } from '@emotion/react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+const homepageContainer = css`
+  background-image: url(/rennerHomePage.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    text-align: center;
+    z-index: 1;
+  }
+
+  h1 {
+    margin: 0;
+    color: white;
+    font-size: 3.5em;
+  }
+
+  button {
+    background: none;
+    color: white;
+    font-size: 3em;
+    border: white 1px solid;
+    padding: 15px 25px;
+    margin-top: 20px;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+  }
+`;
+
+const blueOverlay = css`
+  background: #182b4f;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  opacity: 81%;
+`;
+
 export default function Home() {
   return (
-    <div>
+    <div css={homepageContainer}>
+      <div css={blueOverlay} />
       <Head>
         <title>Home</title>
       </Head>
@@ -12,7 +60,7 @@ export default function Home() {
         <h1>Vienna Vanguards Online Store</h1>
         <button>
           <Link href="/products">
-            <a>Shop Now</a>
+            <a>Shop now</a>
           </Link>
         </button>
       </div>
