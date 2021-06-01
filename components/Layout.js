b480a1bc8317fa49ef6b345598f98935b0c24ceb
Andrea Wöger <andrea.woeger@gmx.net>
2021-06-01 09:29:06 +0200

    Fix ESLint errors

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { css, Global } from '@emotion/react';
+import { css } from '@emotion/react';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from '@fortawesome/fontawesome-svg-core';
 import Footer from './Footer';
