bcf647192ac657e7c64829b650bc75b88967fb77
Andrea Wöger <andrea.woeger@gmx.net>
2021-06-07 23:07:25 +0200

    Style shopping cart

--- pages/shoppingCart.js
+++ pages/shoppingCart.js
@@ -1,5 +1,10 @@
 import { css } from '@emotion/react';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faMinus,
+  faPlus,
+  faShoppingCart,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -11,16 +16,87 @@ import {
   substractProductByProductId,
 } from '../util/cookies';
 
-// Todo: product goes under null - remove
-// Todo: import product pictures, name and price
 // Todo: Calculate total price
+
+const heading = css`
+  font-size: 2em;
+  margin: 1.5em 0;
+`;
+
+const clearAllButton = {};
 
 const noButton = css`
   display: none;
 `;
 
 const buttonQuantity = css`
-  background: grey;
+  background-color: #000000;
+  color: #182b4f;
+  border: none;
+  padding: 5px 8px;
+`;
+
+const mainContainer = css`
+  display: flex;
+`;
+
+const container = css`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  width: 50%;
+
+  > div {
+    display: flex;
+    align-items: center;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+
+  img {
+    width: 20%;
+    margin-right: 20px;
+  }
+
+  h3 {
+    font-size: 1.2em;
+    margin: 0 20px 0 0;
+  }
+
+  h4 {
+    font-size: 1.2em;
+    margin: 0 20px 0 0;
+  }
+
+  p {
+    margin-right: 20px;
+    font-size: 1.2em;
+    color: #182b4f;
+  }
+
+  button {
+    margin-right: 20px;
+    background: white;
+    color: #182b4f;
+    padding: 3px 8px;
+    font-size: 0.9em;
+    border: 2px solid #182b4f;
+  }
+`;
+
+const subContainer = css`
+  display: inline-flex;
+  align-items: center;
+`;
+
+const screenreaderSpans = css`
+  display: none;
+`;
+
+const sumContainer = css`
+  p > span {
+  }
 `;
 
 export default function ShoppingCart(props) {
@@ -42,80 +118,115 @@ export default function ShoppingCart(props) {
       <Head>
         <title>Shopping Cart</title>
       </Head>
-      <h2>Shopping cart</h2>
-      <div>
-        {productsInShoppingCart.map((item) => {
-          return (
-            <div key={item.id}>
-              <div>
+      <h2 css={heading}>Shopping cart</h2>
+      <div css={mainContainer}>
+        <div>
+          {productsInShoppingCart.map((item) => {
+            return (
+              <div css={container} key={item.id}>
                 <img src={item.imgFront} alt={item.productName} />
+                <div>
+                  <div css={subContainer}>
+                    <h3>{item.productName}</h3>
+                    <h4>
+                      {item.price} {item.currency}
+                    </h4>
+                  </div>
+                  <div css={subContainer}>
+                    <p>
+                      Amount:{' '}
+                      {
+                        props.shoppingCart.find((pro) => pro.id === item.id)
+                          ?.quantity
+                      }
+                    </p>
+                    <button
+                      css={
+                        props.shoppingCart.find((pro) => pro.id === item.id)
+                          ?.quantity === 1
+                          ? noButton
+                          : buttonQuantity
+                      }
+                      onClick={() => {
+                        props.setShoppingCart(
+                          substractProductByProductId(item.id),
+                        );
+                      }}
+                    >
+                      <FontAwesomeIcon
+                        size="xs"
+                        icon={faMinus}
+                        aria-hidden="true"
+                        title="Substract item"
+                      />
+                      <span css={screenreaderSpans}>Substract item</span>
+                    </button>
+                    <button
+                      css={buttonQuantity}
+                      onClick={() => {
+                        props.setShoppingCart(addProductByProductId(item.id));
+                      }}
+                    >
+                      <FontAwesomeIcon
+                        size="xs"
+                        icon={faPlus}
+                        aria-hidden="true"
+                        title="Add item"
+                      />
+                      <span css={screenreaderSpans}>Add item</span>
+                    </button>
+                    <button
+                      onClick={() => {
+                        props.setShoppingCart(removeProductById(item.id));
+                      }}
+                    >
+                      <FontAwesomeIcon
+                        size="sm"
+                        icon={faTrashAlt}
+                        aria-hidden="true"
+                        title="Delete item"
+                      />{' '}
+                      <span css={screenreaderSpans}>Delete item</span>
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div>
-                <p>{item.productName}</p>
-
-                <p>
-                  Amount:{' '}
-                  {
-                    props.shoppingCart.find((pro) => pro.id === item.id)
-                      ?.quantity
-                  }
-                </p>
-              </div>
-              <button
-                css={
-                  props.shoppingCart.find((pro) => pro.id === item.id)
-                    ?.quantity === 1
-                    ? noButton
-                    : buttonQuantity
-                }
-                onClick={() => {
-                  props.setShoppingCart(substractProductByProductId(item.id));
-                }}
-              >
-                -
-              </button>
-              <button
-                onClick={() => {
-                  props.setShoppingCart(addProductByProductId(item.id));
-                }}
-              >
-                +
-              </button>
-              <button
-                onClick={() => {
-                  props.setShoppingCart(removeProductById(item.id));
-                }}
-              >
-                Remove item
-              </button>
-              <p>
-                {item.price} {item.currency}
-              </p>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div css={sumContainer}>
+          <div>
+            <span>Total amount of products:</span>{' '}
+            <span>
+              {props.shoppingCart
+                .map((item) => item.quantity)
+                .reduce((total, amount) => total + amount, 0)}
+            </span>
+          </div>
+          <div>
+            <span>Total sum:</span>{' '}
+            <span>
+              {productsInShoppingCart
+                .map((p) => Number(p.price))
+                .reduce((total, amount) => total + amount, 0)}
+              €
+            </span>
+          </div>
+          <div>
+            <button
+              css={clearAllButton}
+              onClick={() => props.setShoppingCart(clearShoppingCart())}
+            >
+              Clear Cart <FontAwesomeIcon size="1x" icon={faShoppingCart} />
+            </button>
+            <Link href="/checkout">
+              <a>
+                <button>Checkout</button>
+              </a>
+            </Link>
+          </div>
+        </div>
       </div>
-      <button onClick={() => props.setShoppingCart(clearShoppingCart())}>
-        Clear Shopping Cart <FontAwesomeIcon size="1x" icon={faShoppingCart} />
-      </button>
-      <Link href="/checkout">
-        <a>
-          <button>Go to checkout</button>
-        </a>
-      </Link>
-      <p>
-        Total amount of products:
-        {props.shoppingCart
-          .map((item) => item.quantity)
-          .reduce((total, amount) => total + amount, 0)}
-      </p>
-      <p>
-        Total sum:{' '}
-        {productsInShoppingCart
-          .map((p) => Number(p.price))
-          .reduce((total, amount) => total + amount, 0)}
-        €
-      </p>
     </Layout>
   );
 }
